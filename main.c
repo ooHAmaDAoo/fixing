f40da36a02309ac4ccccf74c506a88bc5aea6d13
ooHAmaDAoo <107913690+ooHAmaDAoo@users.noreply.github.com>
2023-02-24 01:24:30 +0300

    Update main.c

--- main.c
+++ main.c
@@ -2,22 +2,33 @@
 #include <stdlib.h>
 #include "circlemath.h"
 
-int main() {
+int main()
+{
+    int radius1 = 3, radius2 = 5;
+    
     pid_t pid = fork();
-
-    if (pid == -1) {
+    
+    if (pid == -1)
+    {
         printf("Error forking process.\n");
         return 1;
     }
 
-    if (pid == 0) { // child process
-        int radius1 = 3, radius2 = 5;
+    if (pid == 0)
+    {   // child process
         printf("Radius: %d\n", radius1);
         printf("Area: %.2lf\n", area(radius1));
         printf("Circumference: %.2lf\n", circumference(radius1));
+        
+//        printf("Radius: %d\n", radius2);
+  //      printf("Area: %.2lf\n", area(radius2));
+    //    printf("Circumference: %.2lf\n", circumference(radius2));
+    }
+    else
+    {
         printf("Radius: %d\n", radius2);
         printf("Area: %.2lf\n", area(radius2));
-        printf("Circumference: %.2lf\n", circumference(radius2));
+        printf("Circumference: %.2lf\n", circumference(radius2));   
     }
 
     return 0;
